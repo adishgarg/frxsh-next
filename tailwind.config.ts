bfd944ebfa636b64b457b91bc70da267699a8d69
adishgarg <gargadi456@gmail.com>
2025-01-22 22:21:19 +0530

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,22 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        grid: "grid 15s linear infinite",
+      },
+      keyframes: {
+        grid: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        oswald: ['Oswald','sans-serif' ]
+      }
     },
   },
   plugins: [],
