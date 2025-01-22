fe3f91681d79522e62e5924ba546e0765ce84ade
adishgarg <gargadi456@gmail.com>
2025-01-23 01:20:41 +0530

    eslint

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig:NextConfig = {
   distDir: 'dist', // Set the output directory to 'dist'
   images: {
     domains: ['w16manik.blr1.cdn.digitaloceanspaces.com'], // Add your domain here
+  }, eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint for builds
   },
 };
 
